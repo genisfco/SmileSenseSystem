3cae915a39cd99011e24eef63e3363208ec08335
Maicon Maicon <mdx2000@outlook.pt>
2023-04-12 22:44:51 -0300

    Início da Entidade Odontograma no MVC

---
+++ Controllers/Map/OdontogramaMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+
+namespace Controllers.Map
+{
+    //class OdontogramaMap : EntityTypeConfiguration<Odontograma>
+    //{
+    //    public OdontogramaMap()
+    //    {
+    //        this.ToTable("Odontograma");
+    //        this.HasKey(odt => odt.IdOdontograma);
+    //        this.Property(odt => odt.IdOdontograma).HasColumnName("ID_ODONTOGRAMA");
+    //        this.Property(odt => odt.IdPaciente).HasColumnName("ID_PACIENTE");
+    //        this.Property(odt => odt.Procedimentos).HasColumnName("PROCEDIMENTOS");
+    //        this.Property(odt => odt.Imagem).HasColumnName("IMAGEM");
+    //    }
+    //}
+}
---
+++ Entidades/Odontograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Odontograma
+    {
+        public int IdOdontograma { get; set; }
+        public int IdPaciente { get; set; }
+        public string Procedimentos { get; set; }
+        public string Imagem { get; set; }
+    }
+}
