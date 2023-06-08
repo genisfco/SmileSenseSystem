a23139e33e488abb6428be0645cd2f6b6eb0133a
Maicondexon <maicondexon@yahoo.com.br>
2023-06-08 15:22:05 -0300

    criação de envio de email para suporte no botao enviar

--- SistemaOdonto/FrmSuporte.cs
+++ SistemaOdonto/FrmSuporte.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace SistemaOdonto
 {
@@ -19,7 +22,21 @@ namespace SistemaOdonto
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Mensagem encaminhada para o suporte, aguarde nosso contato.");
+            //
+            try
+            {
+                string destinatario = "maicondexon@gmail.com";
+                string assunto = "Assunto do email";
+                string corpo = "Corpo do email";
+
+                string url = $"mailto:{destinatario}?subject={Uri.EscapeDataString(assunto)}&body={Uri.EscapeDataString(corpo)}";
+
+                Process.Start(url);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao abrir o cliente de email: " + ex.Message);
+            }
         }
     }
 }
