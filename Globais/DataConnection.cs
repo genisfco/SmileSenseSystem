c540417249889e25f430758e9eee3b47484e4849
GENIS OLIVEIRA <genis.oliveira@etec.sp.gov.br>
2023-06-29 13:59:50 -0300

    Update strings conexão

--- Globais/DataConnection.cs
+++ Globais/DataConnection.cs
@@ -11,8 +11,7 @@ namespace Globais
 
         private static SqlConnection ConexaoBanco()
         {
-            string connection_sql = @"Server=ACERASPIRE-5\SQLEXPRESS;Database=SmileSense;Integrated Security=True;";
-            
+            string connection_sql = @"Server=ACERASPIRE-5\SQLEXPRESS;Database=SmileSense;Integrated Security=True;";            
 
             SqlConnection msConnection = new SqlConnection();
             msConnection.ConnectionString = connection_sql;
